44e341734069aafd0d89478094a8cddaa61ebf89
Shymaa Ismail <shymaa.m.ismail@gmail.com>
2025-07-18 20:37:18 +0300

    Create middleware.py

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,11 @@
+from .models import RequestLog
+from django.utils.deprecation import MiddlewareMixin
+from ipware import get_client_ip
+
+class LogIPMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        ip, _ = get_client_ip(request)
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=request.path
+        )
