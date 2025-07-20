8fd2e570cab8007716bb20981aa747bd0d2adbab
shymaaismail <shymaa.m.ismail@gmail.com>
2025-07-20 08:15:03 +0300

    Update .gitignore to include alx_backend_security/__pycache__; add login view to ip_tracking URLs and refactor login function

--- ip_tracking/urls.py
+++ ip_tracking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .api_views import check_ip
-
+from . import views
 urlpatterns = [
     path('check-ip/', check_ip, name='check-ip'),
+    path('login/', views.login, name='login'),
 ]
--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
-from ratelimit.decorators import ratelimit
+from django_ratelimit.decorators import ratelimit
 
 
 @ratelimit(key='ip', rate='10/m', method='POST', block=True)
 @ratelimit(key='ip', rate='5/m', method='GET', block=True)
-def login_view(request):
+def login(request):
     return JsonResponse({"message": "Login successful"})
